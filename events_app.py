d30c4eeac306d6864c9acf05e1b19dc8b79fdbfd
Lauren <ltso@wellesley.edu>
2018-11-22 21:42:23 +0000

    add input type

--- events_app.py
+++ events_app.py
@@ -18,15 +18,45 @@ def viewSubmitted():
 
 @app.route('/submitEvent/', methods=['POST'])
 def submitEvent():
-    conn = events.getConn('c9')
-    name = request.form.get('name')
-    city = request.form.get('city')
-    state = request.form.get('state')
-    country = request.form.get('country')
-    desc = request.form.get('desc')
-    date = request.form.get('date')
-    events.submitEvent(conn, name, city, state, country, desc, date)
-    flash("Event {} submitted for approval by admins".format(name))
+    error = False
+    name = ''
+    city = ''
+    state = ''
+    country = ''
+    description = ''
+    date = ''
+    
+    try:
+        name = request.form.get('name')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        country = request.form.get('country')
+        desc = request.form.get('desc')
+        date = request.form.get('date')
+    except:
+        flash("Access to missing form inputs")
+        error = True
+    
+    if name == '':
+        flash("Missing input: Event's name is missing")
+        error = True
+    
+    checkdate = "".join(request.form.get('date').split("-"))
+    if not checkdate or not checkdate.isdigit():
+        error = True
+        if not checkdate:
+            flash("Missing input: Event's date is missing")
+        else:
+            flash("Date is not numeric")
+    
+    if not error:
+        conn = events.getConn('c9')
+        if events.checkEvent(conn, name, date):
+            flash("Event {} at {} exists".format(name, date))
+        else:
+            events.submitEvent(conn, name, city, state, country, desc, date)
+            flash("Event {} submitted for approval by admins".format(name))
+            
     return redirect(url_for('viewApproved'))
 
 @app.route('/approveEvent/', methods=['POST'])
