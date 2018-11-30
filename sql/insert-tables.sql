41ed5620d1f7fbbb67964db30fface4df411ed25
Lauren <ltso@wellesley.edu>
2018-11-30 21:10:03 +0000

    update tables

--- sql/insert-tables.sql
+++ sql/insert-tables.sql
@@ -3,7 +3,7 @@ use c9;
 insert into user(name,nickname,classyear,username,password,user_type,sprefs) values ("Riann","Nut",2019,"rtang","pass","admin","all"),
 ("Lauren","ebae",2020,"ltso","pass","admin","all"), ("Tam","Briese",2019,"tdeshong","pass","admin","all");
 insert into industry(pid,iname) values ("ltso","Health Professions"), ("tdeshong","Technology and Engineering");
-insert into events(ename,city,state,country,description,edate,approved) values("Millyfest","Wellesley","MA","US","tourney","2018-11-12",1);
+insert into events(ename,city,state,country,description,edate,approved,pid) values("Millyfest","Wellesley","MA","US","tourney","2018-11-12",1,"ltso");
 insert into messages(sender,receiver,message) values ("ltso","tdeshong","hi"), ("ltso","tdeshong","bye"), ("tdeshong","rtang","cool");
 insert into team(tname,`type`,nearestcity,state,country,pid) values ("Brute Squad","club","Boston","MA","US","tdeshong"), ("Nightlock","league","SF","CA","US","ltso");
 insert into location(pid,city,nearestcity,state,country) values ("ltso","NYC","NYC","NY","US");
