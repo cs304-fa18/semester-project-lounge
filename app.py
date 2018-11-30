98e1e7741ac8153e3519318eccba6164666e8623
Lauren <ltso@wellesley.edu>
2018-11-30 22:01:44 +0000

    update userinfo format

--- app.py
+++ app.py
@@ -8,21 +8,20 @@ app.secret_key = "notverysecret"
 def index():
     return render_template('index.html')
 
-@app.route('/preferences/', methods=['POST', 'GET'])
+@app.route('/createAccount/', methods=['POST', 'GET'])
 def logins():
     if request.method == ('GET'):
         return render_template('userinfo.html')
     else:
         email = request.form.get("email")
         uname = request.form.get("username")
-        pwd = request.form.get("password")
         pwd1 = request.form.get("password1")
+        pwd2 = request.form.get("password2")
         name = request.form.get("name")
-        nname = request.form.get("nname")
+        nname = request.form.get("nickname")
         year = request.form.get("year")
-        phnum =request.form.get("pnum")
+        phnum =request.form.get("phnum")
         sprefs = request.form.get("radio")
-        
 
         if email == "" or "@" not in email:
             flash("Invalid email address")
